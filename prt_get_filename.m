67739f366197c73bd3fa96b9358b86a6f5e0dca4
amarquan <amarquan@769004dd-e674-4e6d-a904-089baae6d200>
2011-09-07 14:54:22 +0000

    Minor modifications

--- prt_get_filename.m
+++ prt_get_filename.m
@@ -1,4 +1,4 @@
-function out = prt_get_filename(PRT,g,s,m,c)
+function out = prt_get_filename(ids)
 
 %__________________________________________________________________________
 % Copyright (C) 2011, ...
@@ -9,25 +9,25 @@ function out = prt_get_filename(PRT,g,s,m,c)
 % Obs: Names return to the simple form: e.g. 'g1_s1_m1_c1'
 
 try
-    group_prefix = ['g' int2str(g) '_'];
+    group_prefix = ['g' int2str(ids(1)) '_'];
 catch
     group_prefix = '';
 end
 
 try
-    subj_prefix = ['s' int2str(s) '_'];
+    subj_prefix = ['s' int2str(ids(2)) '_'];
 catch
     subj_prefix = '';
 end
 
 try
-    mod_prefix = ['m' int2str(m)];
+    mod_prefix = ['m' int2str(ids(3))];
 catch
     mod_prefix = '';
 end
 
 try
-    cond_prefix = ['_c' int2str(c)];
+    cond_prefix = ['_c' int2str(ids(4))];
 catch
     cond_prefix = '';
 end
