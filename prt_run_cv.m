f244c41907c58da7f122994d1d44b9f09d6cce88
amarquan <amarquan@769004dd-e674-4e6d-a904-089baae6d200>
2011-09-07 14:55:33 +0000

    Added CV scripts

---
+++ prt_run_cv.m
@@ -0,0 +1,32 @@
+function out = prt_run_cv(varargin)
+% PRONTO job execution function
+% takes a harvested job data structure and rearrange data into "proper"
+% data structure, then save do what it has to do...
+% Here simply the harvested job structure in a mat file.
+%
+% Input:
+% job    - harvested job data structure (see matlabbatch help)
+% Output:
+% out    - filename of saved data structure.
+%__________________________________________________________________________
+% Copyright (C) 2011
+
+% Written by A Marquand
+% $Id: prt_run_kernel_construction.m 33 2011-08-10 08:47:17Z cphillip $
+
+% Job variable
+% -------------------------------------------------------------------------
+job   = varargin{1};
+
+% Load PRT.mat and configure some variables
+% -------------------------------------------------------------------------
+fname = char(job.infile);
+load(fname);
+
+disp('This is just a placeholder. Not implemented yet');
+
+% Function output
+% -------------------------------------------------------------------------
+%out.files{1} = outfile;
+disp('Cross-validation done.')
+end
