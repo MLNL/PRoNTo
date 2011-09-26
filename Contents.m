ba86afbd0de58b86d3bf597c3f8356322eb3c204
jrichiar <jrichiar@769004dd-e674-4e6d-a904-089baae6d200>
2011-09-26 17:37:27 +0000

    skeleton for LaTeX tutorial + start of matlab package Contents.m (good practices)

---
+++ Contents.m
@@ -0,0 +1,29 @@
+% Pattern Recognition for Neuroimaging Toolbox (PRoNTo)
+%
+% $Id$
+% 
+% Files
+%   prt_batch - launch the PRoNTo batch system
+%   prt_cfg_batch - (internal) add PRoNTo menu to batch system
+%   prt_cfg_cv.m - 
+%   prt_cfg_design - builds PRT.mat file containing data and design info
+%   prt_cfg_kernel_construction - 
+%   prt_cfg_preproc - 
+%   prt_check_design -
+%   prt_data_conditions -
+%   prt_data_modality - 
+%   prt_data_review - 
+%   prt_defaults - 
+%   prt_get_defaults - 
+%   prt_get_filename - 
+%   prt_load_blocks - 
+%   prt_normalise_kernel - 
+%   prt_preproc - XXX same as run_preproc
+%   prt_remove_confounds - 
+%   prt_run_cv - 
+%   prt_run_design - 
+%   prt_run_kernel_construction - 
+%   prt_run_preproc - 
+%   prt_text_input - 
+%   prt_ui_design - 
+%   prt_ui_main - 
