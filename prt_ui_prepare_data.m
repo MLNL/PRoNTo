17e048db70bde67d82a6a21bfa296de5a5a0dc69
amarquan <amarquan@769004dd-e674-4e6d-a904-089baae6d200>
2011-10-13 15:08:32 +0000

    Added extra files

---
+++ prt_ui_prepare_data.m
@@ -0,0 +1,248 @@
+function varargout = prt_ui_prepare_data(varargin)
+% PRT_UI_KERNEL MATLAB code for prt_ui_kernel.fig
+%      PRT_UI_KERNEL, by itself, creates a new PRT_UI_KERNEL or raises the existing
+%      singleton*.
+%
+%      H = PRT_UI_KERNEL returns the handle to a new PRT_UI_KERNEL or the handle to
+%      the existing singleton*.
+%
+%      PRT_UI_KERNEL('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in PRT_UI_KERNEL.M with the given input arguments.
+%
+%      PRT_UI_KERNEL('Property','Value',...) creates a new PRT_UI_KERNEL or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before prt_ui_kernel_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to prt_ui_kernel_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help prt_ui_kernel
+
+% Last Modified by GUIDE v2.5 28-Sep-2011 17:30:50
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @prt_ui_prepare_data_OpeningFcn, ...
+                   'gui_OutputFcn',  @prt_ui_prepare_data_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before prt_ui_kernel is made visible.
+function prt_ui_prepare_data_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to prt_ui_kernel (see VARARGIN)
+
+% Choose default command line output for prt_ui_kernel
+handles.output = hObject;
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes prt_ui_kernel wait for user response (see UIRESUME)
+% uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = prt_ui_prepare_data_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+
+
+
+function edit_prt_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_prt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_prt as text
+%        str2double(get(hObject,'String')) returns contents of edit_prt as a double
+fname=get(handles.edit_prt,'String');
+try
+    load(fname)
+    handles.dat=PRT;
+catch
+    beep
+    disp('Could not load file')
+    return
+end
+%if only one modality, than fill some fields automatically
+n_mod=length(PRT.group(1).subject(1).modality);
+handles.modnames={PRT.masks(:).mod_name};
+if n_mod==1
+    set(handles.num_mod,'Value',1)
+    set(handles.num_mod,'String',1)
+    set(handles.sel_mod,'String',{PRT.masks(1).mod_name})
+    handles.mod=prt_ui_prepare_datamod('UserData',{PRT,1});
+end
+handles.fname=fname;
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Executes during object creation, after setting all properties.
+function edit_prt_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_prt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in br_prt.
+function br_prt_Callback(hObject, eventdata, handles)
+% hObject    handle to br_prt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+fname=spm_select(1,'.mat','Select PRT.mat',[],pwd,'PRT.mat');
+try
+    load(fname)
+    handles.dat=PRT;
+    set(handles.edit_prt,'String',fname);
+catch
+    beep
+    disp('Could not load file')
+    return
+end
+%if only one modality, than fill some fields automatically
+n_mod=length(PRT.group(1).subject(1).modality);
+handles.modnames={PRT.masks(:).mod_name};
+if n_mod==1
+    set(handles.num_mod,'Value',1)
+    set(handles.num_mod,'String',1)
+    set(handles.sel_mod,'String',{PRT.masks(1).mod_name})
+    handles.mod=prt_ui_prepare_datamod('UserData',{PRT,1});
+end
+handles.fname=fname;
+% Update handles structure
+guidata(hObject, handles);
+
+
+function edit_kname_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_kname (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_kname as text
+%        str2double(get(hObject,'String')) returns contents of edit_kname as a double
+handles.kname=get(handles.edit_kname,'String');
+% Update handles structure
+guidata(hObject, handles);
+
+
+% --- Executes during object creation, after setting all properties.
+function edit_kname_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_kname (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+function num_mod_Callback(hObject, eventdata, handles)
+% hObject    handle to num_mod (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of num_mod as text
+%        str2double(get(hObject,'String')) returns contents of num_mod as a double
+val=str2double(get(handles.num_mod,'String'));
+n_mod=length(handles.modnames);
+%handles.mod=struct();
+list=[];
+%initialize for all modalities
+for i=1:n_mod
+    handles.mod(i)=struct('mod_name',[],'mode',[],'mask',[],'kernel_dt',[], ...
+        'param_dt',[],'normalise',[],'matnorm',[]);
+end
+%get information for the selected modalities 
+for i=1:val
+    tmp=prt_ui_prepare_datamod('UserData',{handles.dat,i});
+    list=[list,tmp.mod_name];
+    set(handles.sel_mod,'String',list)
+    ind=find(strcmpi(handles.modnames,tmp.mod_name));
+    handles.mod(ind)=tmp;
+end
+% Update handles structure
+guidata(hObject, handles);
+
+
+% --- Executes during object creation, after setting all properties.
+function num_mod_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to num_mod (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+% --- Executes on selection change in sel_mod.
+function sel_mod_Callback(hObject, eventdata, handles)
+% hObject    handle to sel_mod (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = get(hObject,'String') returns sel_mod contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from sel_mod
+
+
+% --- Executes during object creation, after setting all properties.
+function sel_mod_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to sel_mod (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: listbox controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in buildbutt.
+function buildbutt_Callback(hObject, eventdata, handles)
+% hObject    handle to buildbutt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+input=struct('fname',[],'kname',[],'mod',[],'flagk',[]);
+input.fname=handles.fname;
+%input.kname=handles.kname;
+input.fs_name=handles.kname;
+input.mod=handles.mod;
+load(input.fname);
+prt_fs(PRT,input);
+delete(handles.figure1)
---
+++ prt_ui_prepare_datamod.m
@@ -0,0 +1,317 @@
+function varargout = prt_ui_prepare_datamod(varargin)
+% PRT_UI_KERNEL_MODALITY M-file for prt_ui_kernel_modality.fig
+%      PRT_UI_KERNEL_MODALITY, by itself, creates a new PRT_UI_KERNEL_MODALITY or raises the existing
+%      singleton*.
+%
+%      H = PRT_UI_KERNEL_MODALITY returns the handle to a new PRT_UI_KERNEL_MODALITY or the handle to
+%      the existing singleton*.
+%
+%      PRT_UI_KERNEL_MODALITY('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in PRT_UI_KERNEL_MODALITY.M with the given input arguments.
+%
+%      PRT_UI_KERNEL_MODALITY('Property','Value',...) creates a new PRT_UI_KERNEL_MODALITY or raises the
+%      existing singleton*.  Starting from the left, property value pairs are
+%      applied to the GUI before prt_ui_kernel_modality_OpeningFcn gets called.  An
+%      unrecognized property name or invalid value makes property application
+%      stop.  All inputs are passed to prt_ui_kernel_modality_OpeningFcn via varargin.
+%
+%      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
+%      instance to run (singleton)".
+%
+% See also: GUIDE, GUIDATA, GUIHANDLES
+
+% Edit the above text to modify the response to help prt_ui_kernel_modality
+
+% Last Modified by GUIDE v2.5 30-Sep-2011 15:37:09
+
+% Begin initialization code - DO NOT EDIT
+gui_Singleton = 1;
+gui_State = struct('gui_Name',       mfilename, ...
+                   'gui_Singleton',  gui_Singleton, ...
+                   'gui_OpeningFcn', @prt_ui_prepare_datamod_OpeningFcn, ...
+                   'gui_OutputFcn',  @prt_ui_prepare_datamod_OutputFcn, ...
+                   'gui_LayoutFcn',  [] , ...
+                   'gui_Callback',   []);
+if nargin && ischar(varargin{1})
+    gui_State.gui_Callback = str2func(varargin{1});
+end
+
+if nargout
+    [varargout{1:nargout}] = gui_mainfcn(gui_State, varargin{:});
+else
+    gui_mainfcn(gui_State, varargin{:});
+end
+% End initialization code - DO NOT EDIT
+
+
+% --- Executes just before prt_ui_kernel_modality is made visible.
+function prt_ui_prepare_datamod_OpeningFcn(hObject, eventdata, handles, varargin)
+% This function has no output args, see OutputFcn.
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+% varargin   command line arguments to prt_ui_kernel_modality (see VARARGIN)
+
+% Choose default command line output for prt_ui_kernel_modality
+handles.output = hObject;
+set(handles.par_name,'Visible','off')
+set(handles.par_value,'Visible','off')
+set(handles.pop_det,'String',{'No','Linear', ...
+    'Polynomial','Discrete Cosine Transform'})
+set(handles.pop_det,'Value',1)
+set(handles.pop_norm,'String',{'No','Kernel', ...
+    'Specify from .mat'});
+set(handles.pop_norm,'Value',1)
+if ~isempty(varargin{1}) && strcmpi(varargin{1},'UserData')
+    handles.PRT=varargin{2}{1};
+else
+    beep
+    disp('Select a PRT.mat first')
+    return
+end
+mod_n={handles.PRT.masks(:).mod_name};
+set(handles.pop_mod,'String',mod_n)
+set(handles.pop_mod,'Value',1)
+set(handles.figure1,'Name',['Specify modality ',varargin{2}{2}])
+set(handles.pop_cond,'String',{'All conditions','All scans'})
+set(handles.pop_cond,'Value',2)
+handles.mod=struct('mod_name',[],'mode',[],'mask',[],'kernel_dt',[], ...
+        'param_dt',[],'normalise',[],'matnorm',[]);
+handles.mod.mod_name=mod_n(1);
+handles.mod.mode='all_scans';
+handles.mod.kernel_dt=0;
+handles.mod.normalise=0;
+handles.mod.mask=[];
+% Update handles structure
+guidata(hObject, handles);
+
+% UIWAIT makes prt_ui_kernel_modality wait for user response (see UIRESUME)
+uiwait(handles.figure1);
+
+
+% --- Outputs from this function are returned to the command line.
+function varargout = prt_ui_prepare_datamod_OutputFcn(hObject, eventdata, handles) 
+% varargout  cell array for returning output args (see VARARGOUT);
+% hObject    handle to figure
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Get default command line output from handles structure
+varargout{1} = handles.output;
+% The figure can be deleted now
+delete(handles.figure1);
+
+
+
+function edit_mask_Callback(hObject, eventdata, handles)
+% hObject    handle to edit_mask (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of edit_mask as text
+%        str2double(get(hObject,'String')) returns contents of edit_mask as a double
+handles.mod.mask=get(handles.edit_mask,'String');
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Executes during object creation, after setting all properties.
+function edit_mask_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to edit_mask (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in br_mask.
+function br_mask_Callback(hObject, eventdata, handles)
+% hObject    handle to br_mask (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+handles.mod.mask=spm_select(1,'image','Select mask for the selected modality');
+set(handles.edit_mask,'String',handles.mod.mask);
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Executes on selection change in pop_cond.
+function pop_cond_Callback(hObject, eventdata, handles)
+% hObject    handle to pop_cond (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = get(hObject,'String') returns pop_cond contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from pop_cond
+val=get(handles.pop_cond,'Value');
+if val==1
+    handles.mod.mode='all_cond';
+else
+    handles.mod.mode='all_scans';
+end
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Executes during object creation, after setting all properties.
+function pop_cond_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to pop_cond (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in pop_det.
+function pop_det_Callback(hObject, eventdata, handles)
+% hObject    handle to pop_det (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = get(hObject,'String') returns pop_det contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from pop_det
+val=get(handles.pop_det,'Value')-1;
+handles.mod.kernel_dt=val;
+if val==0 % No detrend
+    set(handles.par_name,'Visible','off')
+    set(handles.par_value,'Visible','off')
+    handles.mod.param_dt=[];
+elseif val==1 % Linear detrend
+    set(handles.par_name,'Visible','off')
+    set(handles.par_value,'Visible','off')
+    handles.mod.param_dt=1;
+elseif val==2
+    set(handles.par_name,'Visible','on')
+    set(handles.par_value,'Visible','on')
+    set(handles.par_name,'String','Order')
+    set(handles.par_value,'String','1')
+    handles.mod.param_dt=1;
+elseif val==3
+    set(handles.par_name,'Visible','on')
+    set(handles.par_value,'Visible','on')
+    set(handles.par_name,'String','Cutoff (Hz)')
+    set(handles.par_value,'String','1/128')
+    handles.mod.param_dt=1/128;
+end
+% Update handles structure
+guidata(hObject, handles);
+
+
+% --- Executes during object creation, after setting all properties.
+function pop_det_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to pop_det (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in pop_mod.
+function pop_mod_Callback(hObject, eventdata, handles)
+% hObject    handle to pop_mod (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = get(hObject,'String') returns pop_mod contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from pop_mod
+val=get(handles.pop_mod,'Value');
+list=get(handles.pop_mod,'String');
+handles.mod.mod_name=list(val);
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Executes during object creation, after setting all properties.
+function pop_mod_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to pop_mod (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+
+function par_value_Callback(hObject, eventdata, handles)
+% hObject    handle to par_value (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: get(hObject,'String') returns contents of par_value as text
+%        str2double(get(hObject,'String')) returns contents of par_value as
+%        a double
+handles.mod.param_dt=get(handles.paramd,'Value');
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Executes during object creation, after setting all properties.
+function par_value_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to par_value (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: edit controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on selection change in pop_norm.
+function pop_norm_Callback(hObject, eventdata, handles)
+% hObject    handle to pop_norm (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+% Hints: contents = get(hObject,'String') returns pop_norm contents as cell array
+%        contents{get(hObject,'Value')} returns selected item from pop_norm
+val=get(handles.pop_norm,'Value')-1;
+handles.mod.normalise=val;
+if val==0 || val==1
+    handles.mod.matnorm=[];
+elseif val==2
+    handles.mod.matnorm=spm_select(1,'mat','Select .mat file containing scaling');
+end
+% Update handles structure
+guidata(hObject, handles);
+
+% --- Executes during object creation, after setting all properties.
+function pop_norm_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to pop_norm (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+% Hint: popupmenu controls usually have a white background on Windows.
+%       See ISPC and COMPUTER.
+if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
+    set(hObject,'BackgroundColor','white');
+end
+
+
+% --- Executes on button press in okbutt.
+function okbutt_Callback(hObject, eventdata, handles)
+% hObject    handle to okbutt (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+if strcmpi(handles.mod.mode,'all_cond') && handles.mod.kernel_dt~=0
+    beep
+    disp('The "all conditions" mode can be selected only when no detrending is performed')
+    return
+end
+    
+handles.output=handles.mod;
+% Update handles structure
+guidata(hObject, handles);
+
+uiresume(handles.figure1)
