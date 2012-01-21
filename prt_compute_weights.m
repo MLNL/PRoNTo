626a80c2a38e6b1c93961a2b4a53016b85155cba
mjrosa <mjrosa@769004dd-e674-4e6d-a904-089baae6d200>
2012-01-21 21:24:38 +0000

    Slightly more efficient version.

--- prt_compute_weights.m
+++ prt_compute_weights.m
@@ -19,24 +19,24 @@ for i = 1:nmodel
 end
 % Check if model exists
 if model_idx == 0, error('prt_compute_weights:ModelNotFound',...
-            'Error: model not found in PRT.mat!'); end
-    
+        'Error: model not found in PRT.mat!'); end
+
 % Find machine
 % -------------------------------------------------------------------------
 mfunc       = PRT.model(model_idx).input.machine.function;
 m.args      = [];
 m.function  = 'prt_weights_bin_linkernel';
 
-% unfortunately a bug somewhere causes shifts in weight image if 
+% unfortunately a bug somewhere causes shifts in weight image if
 % .nii is used...
 switch mfunc
-    case 'prt_machine_svm_bin' 
+    case 'prt_machine_svm_bin'
         img_mach    = 'svm_weights.img';
     case 'prt_machine_rvr'
         img_mach    = 'rvr_weights.img';
     case 'prt_machine_krr'
         img_mach    = 'krr_weights.img';
-    % weights computation not yet supported for RT
+        % weights computation not yet supported for RT
     case 'prt_machine_RT_bin'
         error('prt_compute_weights:MachineNotSupported',...
             'Error: weights computation not supported for this machine!');
@@ -64,7 +64,6 @@ end
 % Other info
 % -------------------------------------------------------------------------
 fs_name  = PRT.model(model_idx).input.fs(1).fs_name;
-cvmat    = PRT.model(model_idx).input.cv_mat;
 samp_idx = PRT.model(model_idx).input.samp_idx;
 nfold    = length(PRT.model(model_idx).output.fold);
 
@@ -86,13 +85,12 @@ for i = 1:nfas
         fas_idx = i;
     end
 end
-idfeat = PRT.fas(fas_idx).idfeat_img;
 
 % Create image
 % -------------------------------------------------------------------------
 hdr        = PRT.fas(fas_idx).hdr.private;
 img4d      = file_array(img_name,[hdr.dat.dim(1),hdr.dat.dim(2),...
-             hdr.dat.dim(3),nfold+1],'float64-le',0,1,0);         
+    hdr.dat.dim(3),nfold+1],'float64-le',0,1,0);
 nvox       = hdr.dat.dim(1)*hdr.dat.dim(2)*hdr.dat.dim(3);
 img3dav    = zeros(1,nvox); % average weight map
 
@@ -100,15 +98,15 @@ for f = 1:nfold
     
     disp(sprintf('Computing weights: fold %d of %d',f,nfold))
     
-    train_idx      = cvmat(:,f)==1;
+    train_idx      = PRT.model(model_idx).input.cv_mat(:,f)==1;
     train          = samp_idx(train_idx);
-   
+    
     alphas         = PRT.model(model_idx).output.fold(f).alpha;
     
     d.coeffs       = alphas;
     d.datamat      = PRT.fas(fas_idx).dat(train,:);
     
-    % Apply any operations specified during training 
+    % Apply any operations specified during training
     ops = PRT.model(model_idx).input.operations(PRT.model(model_idx).input.operations ~=0 );
     cvdata.train      = {d.datamat};
     cvdata.tr_id      = ID(train_idx,:);
@@ -122,20 +120,20 @@ for f = 1:nfold
     wimg           = wimg/norm(wimg,2); % normalise weights
     
     img3d          = zeros(1,nvox);
-    img3d(idfeat)  = wimg;
+    img3d(PRT.fas(fas_idx).idfeat_img)  = wimg;
     
     img3dav        = img3dav + img3d;
     
     img4d(:,:,:,f) = reshape(img3d,hdr.dat.dim(1),hdr.dat.dim(2),...
-                     hdr.dat.dim(3),1);      
-   
+        hdr.dat.dim(3),1);
+    
 end
 
 % Create average fold
 %--------------------------------------------------------------------------
 disp('Computing averaged weights')
 img4d(:,:,:,nfold+1) = reshape(img3dav,hdr.dat.dim(1),hdr.dat.dim(2),...
-                     hdr.dat.dim(3),1)/nfold;
+    hdr.dat.dim(3),1)/nfold;
 
 % Create weigths file
 %--------------------------------------------------------------------------
