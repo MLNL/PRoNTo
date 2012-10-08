d4fdd6b266b995e5778a26ed10b8dbc45fe84d43
cphillip <cphillip@769004dd-e674-4e6d-a904-089baae6d200>
2012-10-08 15:48:38 +0000

    Adding check with Faces data, all 3 checks working on my PC. :-)

--- prt_check.m
+++ prt_check.m
@@ -1,4 +1,4 @@
-function ok = prt_check(list_check)
+function ok = prt_check(list_check,dir_root)
 % Function to automatically test PRoNTo's integrity
 %
 % The goal is to have PRoNTo run through typical analysis and check if the
@@ -14,32 +14,43 @@ function ok = prt_check(list_check)
 %
 % See the subfunctions for a detailed description of the tests performed.
 %
-% FORMAT ok = prt_check(list_check)
+% FORMAT ok = prt_check(list_check,dir_root)
 %
 % INPUT
-%   list_check - list of data sets to use
+%   list_check  - list of data sets to use
+%   dir_root    - root directory of data sets
 %
 % OUTPUT:
-%   ok         - vector of output, 1=ok, 0=failed, -1=not tested
+%   ok          - vector of output (1=ok, 0=failed, -1=not tested)
 %
 % NOTE:
-% This will close all Matlab windows before relaunching PRoNTo and the
-% matlabbatch system.
+% - For a more automatic testing on your own system, then up date the
+%   default 'dir_root' variable with the path to the 'PRoNTo_data'
+%   directory on your system.
+% - This will close all Matlab windows before relaunching PRoNTo and the
+%   matlabbatch system.
+% - Need to include the RandomForest classification for a more complete 
+%   check, when available and working. Simple case could be to use RF on 
+%   the Faces data set.
 %_______________________________________________________________________
 % Copyright (C) 2012 Machine Learning & Neuroimaging Laboratory
 
 % Written by Christophe Phillips, CRC, ULg, Belgium.
 % $Id$
 
-% 
+% Defining the check parameters.
+% data sets, list to check and root directory
+%--------------------------------------------
 % list here the ones you want to check, e.g. 'list_check = 1:3;' for all
-if nargin<1, list_check = 1; end
-
-% Defining the data sets and their root directory
-%------------------------------------------------
-dir_root = 'D:\3_Data\PRoNTo\PRoNTo_data';
-% % or select the root directories manually
-% P = spm_select([1 1],'dir','Select root dir for data sets');
+if nargin<1, list_check = [1 2 3]; end
+if nargin<2
+     % adjust with your own data set
+    dir_root = 'D:\3_Data\PRoNTo\PRoNTo_data'; 
+end
+while isempty(dir_root) || ~exist(dir_root,'dir')
+    % or select the root directories manually
+    dir_root = spm_select([1 1],'dir','Select root dir for data sets');
+end
 dat_name = {'Haxby' , 'IXI ' , 'Faces'};
 Ndat = numel(dat_name);
 data_dir = cell(Ndat,1);
@@ -69,6 +80,8 @@ for ii=list_check
     end
 end
 
+% Printing out the results
+%-------------------------
 fprintf('\nTesting on data sets:\n')
 for ii=1:Ndat
     switch ok(ii)
@@ -86,10 +99,11 @@ end
 %% INDIVIDUAL DATA SET CHECKING ROUTINES
 %==========================================================================
 
-% HAXBY data set
+%% HAXBY data set
 function ok = check_Haxby(rdata_dir)
 %
-% This batch will go through the following modules:
+% This batch will go through the following modules, as saved in the
+% batch_test_HaxbyData.mat file:
 % - 'File selector' for (1) images, (2) SPM.mat, (3) mask 1st level, and
 %   (4) mask 2nd level
 % - 'Directory selector' for the root of the data directory
@@ -117,7 +131,6 @@ m_dir = fullfile(rdata_dir,'masks');
 
 % load batch file
 b_file = fullfile(prt('dir'),'_unitTests','batch_test_HaxbyData.mat');
-% b_file = fullfile(rdata_dir,'batch_test2.mat');
 load(b_file)
 
 % set files: (1) images, (2) SPM.mat, (3) mask 1st level, (4) mask 2nd level
@@ -141,34 +154,43 @@ end
 end
 
 %==========================================================================
-% IXI data set
+%% IXI data set
 function ok = check_IXI(rdata_dir)
 %
-% This batch will go through the following modules:
-% - 'File selector' for the images: young-momentum (g1m1), 
-%   young-divergence (g1m2), old-momentum (g2m1), old-divergence (g2m2)
+% This batch will go through the following modules, as saved in the
+% batch_test_IXIdata.mat file:
+% - 'File selector' for the 6 sets of images: 
+%       Guys-divergence (g1m1), Guys-momentum (g1m2), 
+%       HammersH-divergence (g2m1), HammersH-momentum (g2m2)
+%       IOP-divergence (g3m1), IOP-momentum (g3m2)
 % - 'File selector' for 2 masks: momentum, divergence (could be the same 
-%   file actually)
+%   file actually and it will be defined so when filling the batch)
 % - 'Directory selector' for the root of the data directory
 % - 'Make directory', create 'test_results' directory at the root of the 
 %   data directory
-% - 'Data & design' such that 2 groups ('young'/'old') with 2 modalities
-%   each ('momentum'/'divergence', regression target for 'old-divergence'
-% - 'Feature set', only the 'momentum' data
-% - 'Specify model', svm young-vs-old, on momentum data, leave-1s/gr-out
-% - 'Run model'
-% - 'Compute weights' -> create 'svm_YvsO' image
+% - 'Data & design' with 3 groups (Guys/HammersH/IOP) and 2 modalities each
+%   ('momentum'/'divergence'). No regression target defined here! They'll
+%   be defined in the check function.
 % - 'Feature set', only the 'divergence' data
-% - 'Specify model', KRR for age of old-divergence
+% - 'Specify model', svm Guys-vs-(HammersH+IOP), on divergence data, 
+%   leave-1s/gr-out CV
 % - 'Run model'
-% - 'Feature set', pool 'momentum' and 'divergence' data
-% - 'Specify model', GPC young-vs-old, leave-1s/gr-out
+% - 'Compute weights' -> create 'svm_GvsHI' image
+% - 'Feature set', only the 'momentum' data
+% - 'Specify model', KRR for age of all 3 groups of scans
+% - 'Run model'
+% - 'Feature set', pool 'momentum' and 'divergence' data together
+% - 'Specify model', GPC Guys-vs-HammersH-vs-IOP, on 'momentum+divergence'
+%   data leave-1s/gr-out
+% - 'Run model'
+% - 'Specify model', RVR for age of all 3 groups of scans (using momentum
+%   feature set)
+% - 'Run model'
+% - 'Specify model', GPR for age of all 3 groups of scans (using momentum
+%   feature set)
 % - 'Run model'
 
-% TODO:
-% specify the regressors !!!
-
-% % select images and mask(s)
+% set images directories and select mask(s)
 d_dir{1} = fullfile(rdata_dir,'divergences');
 d_dir{2} = fullfile(rdata_dir,'momentum');
 m_dir = fullfile(prt('dir'),'masks');
@@ -191,13 +213,15 @@ matlabbatch{1}.cfg_basicio.cfg_named_file.files{5} = cellstr(...
     spm_select('FPList',fullfile(d_dir{1},'IOP'),'^sdv.*\.nii$')); 
 matlabbatch{1}.cfg_basicio.cfg_named_file.files{6} = cellstr(...
     spm_select('FPList',fullfile(d_dir{2},'IOP'),'^sa.*\.nii$')); 
+
 % set files: mask (1st level) for both modalities
 matlabbatch{2}.cfg_basicio.cfg_named_file.files{1} = cellstr(msk_file);
 matlabbatch{2}.cfg_basicio.cfg_named_file.files{2} = cellstr(msk_file);      
+
 % set directory where result directory is created = "root data directory"
 matlabbatch{3}.cfg_basicio.cfg_named_dir.dirs{1} = {rdata_dir};
 
-% setting regression target for momentum data
+% set regression target for momentum data (modality#2)
 load(fullfile(rdata_dir,'reg_targets','rt_Guys.mat'))
 matlabbatch{5}.prt.data.group(1).select.modality(2).rt_subj = rt;
 load(fullfile(rdata_dir,'reg_targets','rt_HammerH.mat'))
@@ -218,11 +242,53 @@ end
 end
 
 %==========================================================================
-% FvsNF data set
+%% Faces Famous-vs-NonFamous data set
 function ok = check_FvsNF(rdata_dir)
 
-rdata_dir;
+% This batch will go through the following modules, as saved in the
+% batch_test_FacesData.mat file:
+% - 'File selector' for 
+%       (1/2) the 2 sets of images: 'Famous' and 'NonFamous' beta's
+%       (3) the whole brain mask
+% - 'Directory selector' for the root of the data directory
+% - 'Make directory', create 'test_results' directory at the root of the 
+%   data directory
+% - 'Data & design', defining the 2 groups Famous/NonFamous and data
+% - 'Feature set', using the beta images
+% - 'Specify model', svm F-vs-NF, leave-1s-out CV
+% - 'Run model'
+% - 'Compute weights' -> create 'svm_FvsNF' image
+% - 'Specify model', gpc F-vs-NF, leave-1s/gr-out CV
+% - 'Run model'
+% - 'Compute weights' -> create 'gpc_FvsNF' image
+
+% select images, SPM.mat and mask(s)
+d_dir = fullfile(rdata_dir,'Famous');
+[imgF_files] = spm_select('FPList',d_dir,'^beta.*\.img$');
+d_dir = fullfile(rdata_dir,'NonFamous');
+[imgNF_files] = spm_select('FPList',d_dir,'^beta.*\.img$');
+m_dir = fullfile(prt('dir'),'masks');
+[msk_file] = spm_select('FPList',m_dir,'^.*\.img$');
+
+% load batch file
+b_file = fullfile(prt('dir'),'_unitTests','batch_test_FacesData.mat');
+load(b_file)
+
+% set files: (1) images, (2) SPM.mat, (3) mask 1st level, (4) mask 2nd level
+matlabbatch{1}.cfg_basicio.cfg_named_file.files{1} = cellstr(imgF_files);
+matlabbatch{1}.cfg_basicio.cfg_named_file.files{2} = cellstr(imgNF_files);      
+matlabbatch{1}.cfg_basicio.cfg_named_file.files{3} = cellstr(msk_file); 
+% set directory where result directory is created = "root data directory"
+matlabbatch{2}.cfg_basicio.cfg_named_dir.dirs{1} = {rdata_dir};
+
 ok = 1;
+try
+    cfg_util('run',matlabbatch)
+catch ME
+    disp(ME.message)
+    ok = 0;
+    return;
+end
 
 end
 
