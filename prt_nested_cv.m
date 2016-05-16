50d0dfd88f65f4c0b77c0638ac9cebbe8086b2db
jmmonteiro <joao.monteiro@ucl.ac.uk>
2016-05-16 12:06:50 +0100

    Bugfix. Make it compatible with confound removal

--- prt_nested_cv.m
+++ prt_nested_cv.m
@@ -109,7 +109,10 @@ for i = 1:size(par, 2)
         fold.Phi_all = in.Phi_all;
         fold.t       = in.t;
         fold.mid     = in.mid;
-        
+        if isfield(in, 'cov')
+            fold.cov     = in.cov;
+        end
+
         [model, targets] = prt_cv_fold(PRT,fold);
         
         %for classification check that for each fold, the test targets have been trained
