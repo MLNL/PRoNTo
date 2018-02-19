0e9bda81dae2fe2f68d09542663ea2c75cba1225
Jessica Schrouff <jschrouff@ulg.ac.be>
2018-02-19 16:41:48 +0000

    MTL machine and code in progress
    1st draft at interfacing MALSAR toolbox for MTL.  Machine to test and code to amend for CV and hyper-parameter selection

---
+++ machines/prt_machine_MTL_MALSAR.m
@@ -0,0 +1,149 @@
+function output = prt_machine_MTL_MALSAR(d,args)
+% Run binary SVM - wrapper for LIBLINEAR
+% FORMAT output = prt_machine_L1svm(d,args)
+% Inputs:
+%   d         - structure with data information, with mandatory fields:
+%     .train      - training data (cell array of matrices of row vectors,
+%                   each [Ntr x D]). each matrix contains one representation
+%                   of the data. This is useful for approaches such as
+%                   multiple kernel learning.
+%     .test       - testing data  (cell array of matrices row vectors, each
+%                   [Nte x D])
+%     .tr_targets - training labels (for classification) or values (for
+%                   regression) (column vector, [Ntr x 1])
+%     .te_targets - testing labels (for classification) or values (for
+%                   regression) (column vector, [Ntr x 1])
+%    args         - Regularization parameter
+% Output:
+%    output  - output of machine (struct).
+%     * Mandatory fields:
+%      .predictions - predictions of classification or regression [Nte x D]
+%     * Optional fields:
+%      .func_val - value of the decision function
+%      .type     - which type of machine this is (here, 'classifier')
+%__________________________________________________________________________
+% Copyright (C) 2011 Machine Learning & Neuroimaging Laboratory
+
+% Written by J. Schrouff from prt_machine_svm_bin.m
+% $Id$
+
+SANITYCHECK=true; % can turn off for "speed". Expert only.
+
+%Regularization parameter
+if isempty(args)
+    def = prt_get_defaults('model');
+    args = def.MTLargs;
+end
+
+if SANITYCHECK==true
+%     % args should be a string (empty or otherwise)
+%     if ~ischar(args)
+%         error('prt_machine_L1svm:liblinearargsNotString',['Error: liblinear'...
+%             ' args should be a string. ' ...
+%             ' SOLUTION: Please do XXX']);
+%     end
+    
+%     % check we can reach the binary library
+%     if ~exist('train','file')
+%         error('prt_machine_L1svm:libNotFound',['Error:'...
+%             ' liblinear train function could not be found !' ...
+%             ' SOLUTION: Please check your path.']);
+%     end
+    % check it is indeed a two-class classification problem
+    uTL=unique(d.tr_targets{1}(:));
+    nC=numel(uTL);
+    if nC>2
+        error('prt_machine_MTL_MALSAR:problemNotBinary',['Error:'...
+            ' This machine is only for two-class problems but the' ...
+            ' current problem has ' num2str(nC) ' ! ' ...
+            'SOLUTION: Please select another machine than ' ...
+            'prt_machine_MTL_MALSAR in XXX']);
+    end
+    % check it is indeed labelled correctly (probably should be done) 
+    if ~all(uTL==[1 2]')
+        error('prt_machine_MTL_MALSAR:LabellingIncorect',['Error:'...
+            ' This machine needs labels to be in {1,2} ' ...
+            ' but they are ' mat2str(uTL) ' ! ' ...
+            'SOLUTION: Please relabel your classes by changing the '...
+            ' ''tr_targets'' argument to prt_machine_MTL_MALSAR']);
+    end
+    
+    % check we are using the C-SVC (exclude types -s 1,2,3,4)
+%     if ~isempty(regexp(args,'-s\s+[1234]','once'))
+%         error('prt_machine_svm_bin:argsProblem:onlyCSVCsupport',['Error:'...
+%             ' This machine only supports a C-SVC formulation ' ...
+%             ' (''-s 0'' in the ''args'' parameter), but the args ' ...
+%             ' supplied are ''' args ''' ! ' ...
+%             'SOLUTION: Please change the offending part of args to '...
+%             '''-s 0''']);
+%     end
+    
+    % check we are using linear or precomputed kernels
+    % (exclude types -t 1,2,3)
+%     if ~isempty(regexp(args,'-t\s+[123]','once'))
+%         error('prt_machine_svm_bin:argsProblem:onlyLinOrPrecomputeSupport',...
+%             ['Error: This machine only supports linear or precomputed ' ...
+%             'kernels (''-t 0/4'' in the ''args'' parameter), but the args ' ...
+%             ' supplied are ''' args ''' ! ' ...
+%             'SOLUTION: Please change the offending part of args to '...
+%             '''-t 0'' or ''-t 4'' as intended']);
+%     end
+    
+end
+
+
+% Run Multi-Task in MALSAR
+%--------------------------------------------------------------------------
+nt  = numel(d.tr_targets);
+
+%Machine specific inputs
+for t = 1:nt
+    % Change targets to be +1/-1
+    c2 = d.tr_targets{t}==2;
+    d.tr_targets{t}(c2) = -1;
+    % Add bias
+    d.train{t} = [d.train{t}, ones(size(d.train{t},1),1)];
+    d.test{t} = [d.test{t}, ones(size(d.test{t},1),1)];
+end
+
+opts.init = 0; % compute start point from data.
+opts.tFlag = 0; % terminate after objective value does not changes much.
+opts.tol = 10^-5; % tolerance.
+opts.maxIter = 1500; % maximum iteration number of optimization.
+
+[W,C] = Logistic_L21(d.train,d.tr_targets,args,opts);
+
+
+% check if training succeeded:
+if isempty(W) || ~nnz(W)
+    error('prt_machine_MTL_MALSAR:MALSARtrainUnsuccessful',['Error:'...
+        ' MALSAR LogisticL21 function did not run properly!' ...
+        ' This could be a problem with the supplied function arguments'...
+        ' ' num2str(args) '']);
+end
+
+predictions = cell(1,nt);
+func_val = cell(1,nt);
+for t = 1:nt
+    func_val{t} = d.test{t} * W(:, t) + C(t);
+    pred = sign(func_val{t});
+    % change predictions from 1/-1 to 1/2 
+    c1PredIdx               = pred==1; 
+    pred(c1PredIdx)  = 1; %positive values = 1 
+    pred(~c1PredIdx) = 2; %negative values = 2 
+    predictions{t} = pred;
+end
+
+
+
+
+% Outputs
+%--------------------------------------------------------------------------
+output.predictions = predictions;
+output.func_val    = func_val;
+output.type        = 'classifier';
+output.w           = W;
+output.b           = C;
+
+end
+
---
+++ prt_cv_MTL.m
@@ -0,0 +1,217 @@
+function [outfile] = prt_cv_MTL(PRT,in)
+
+% Function to run a cross-validation structure on a given model
+%
+% Inputs:
+% -------
+% PRT:             data structure
+% in.fname:        filename for PRT.mat (string)
+% in.model_name:   name for this model (string)
+% in.models_MTL:   names of models to combine (cell array)
+%
+% Outputs:
+% --------
+% Writes the following fields in the PRT data structure:
+%
+% PRT.model(m).output.fold(i).targets:     targets for fold(i)
+% PRT.model(m).output.fold(i).predictions: predictions for fold(i)
+% PRT.model(m).output.fold(i).stats:       statistics for fold(i)
+% PRT.model(m).output.fold(i).{custom}:    optional fields
+%
+% Notes:
+% ------
+% The PRT.model(m).input fields are set by prt_init_model, not by
+% this function
+%
+%__________________________________________________________________________
+% Copyright (C) 2011 Machine Learning & Neuroimaging Laboratory
+
+% Written by J. Schrouff based on prt_cv_model
+% $Id$
+
+% prt_dir = char(regexprep(in.fname,'PRT.mat', ''));
+
+% Get index of specified MTL model
+mid = prt_init_model(PRT, in);
+
+% Get index of STL models to combine
+nt = numel(in.models_MTL); %number of tasks
+m = zeros(nt,1);
+for t=1:nt
+    in.model_name = in.models_MTL{t};
+    m(t) = prt_init_model(PRT, in);
+end
+
+% For each task, gather information
+% configure some variables
+CV       = PRT.model(mid).input.cv_mat;     % CV matrix
+n_folds  = size(CV,2);                      % number of CV folds
+
+% targets
+if isfield(PRT.model(mid).input,'include_allscans') && ...
+        PRT.model(mid).input.include_allscans
+    t = PRT.model(mid).input.targ_allscans;
+    % Get covariates if GLM required
+    if any(ismember(PRT.model(mid).input.operations,5))
+        cov = PRT.model(mid).input.cov_allscans;
+    else
+        cov=[];
+    end
+else
+    t = PRT.model(mid).input.targets;
+    % Get covariates if GLM required
+    if any(ismember(PRT.model(mid).input.operations,5))
+        cov = PRT.model(mid).input.covar;
+    else
+        cov=[];
+    end
+end
+
+%get number of classes
+if strcmpi(PRT.model(mid).input.type,'classification')
+    nc=max(unique(t));
+else
+    nc=[];
+end
+fdata.nc = nc;
+
+
+if ~isfield(in,'opt_Rep')
+    opt_Rep = 0;
+end
+
+if PRT.model(mid).input.use_kernel
+    %load kernels and get the used sample in this model
+    [Phi_all,ID] = prt_getKernelModel(PRT,prt_dir,mid,indmodels);
+else
+    [Phi_all,ID] = prt_getFeatureModel(PRT,mid);
+end
+
+% Gather machine string parameters if any
+if ~ isempty(PRT.model(mid).input.machine.args) && ...
+        ischar(PRT.model(mid).input.machine.args)
+    stringpar = PRT.model(mid).input.machine.args;
+else
+    stringpar = [];
+end
+
+% Begin cross-validation loop
+% -------------------------------------------------------------------------
+PRT.model(mid).output=struct();
+
+PRT.model(mid).output.fold = struct();
+for f = 1:n_folds
+    disp ([' > running CV fold: ',num2str(f),' of ',num2str(n_folds),' ...'])
+    % configure data structure for prt_cv_fold
+    fdata.ID      = ID;
+    fdata.mid     = mid; %index of model
+    fdata.CV      = CV(:,f);
+    if isfield(PRT.model(mid).input,'class')
+        fdata.class   = PRT.model(mid).input.class; %to build inner CV for classification special cases
+    end
+
+    fdata.Phi_all = Phi_all; %all kernels
+    fdata.t       = t; %targets
+    if ~isempty(cov)
+        fdata.cov = cov;
+    end
+    fdata.opt_Rep = opt_Rep;
+    
+    % Nested CV for hyper-parameter optimisation or feature selection
+    if isfield(PRT.model(mid).input,'use_nested_cv')
+        if PRT.model(mid).input.use_nested_cv
+            if f==1 && isempty(PRT.model(mid).input.nested_param)
+                beep
+                warning('No parameter range specified for optimization, using defaults.')
+            end
+            if ~isempty(stringpar) %Reset to string before optimization
+                PRT.model(mid).input.machine.args = stringpar;
+            end
+            [out] = prt_nested_cv(PRT, fdata);
+            PRT.model(mid).output.fold(f).param_effect = out;
+            if isempty(stringpar)
+                PRT.model(mid).input.machine.args = out.opt_param;
+            else
+                PRT.model(mid).input.machine.args = [stringpar, num2str(out.opt_param)];
+            end
+        end
+    end
+    
+    % compute the model for this CV fold
+    [model, targets] = prt_cv_fold(PRT,fdata);
+    
+    %for classification check that for each fold, the test targets have been trained
+    if strcmpi(PRT.model(mid).input.type,'classification')
+        if ~all(ismember(unique(targets.test),unique(targets.train)))
+            beep
+            disp('At least one class is in the test set but not in the training set')
+            disp('Abandoning modelling, please correct class selection/cross-validation')
+            return
+        end
+    end
+    
+    % compute stats
+    stats = prt_stats(model, targets.test, nc); %targets.train
+    
+    % update PRT
+    PRT.model(mid).output.fold(f).targets     = targets.test;
+    PRT.model(mid).output.fold(f).predictions = model.predictions(:);
+    PRT.model(mid).output.fold(f).stats       = stats;
+    % copy other fields from the model
+    flds = fieldnames(model);
+    for fld = 1:length(flds)
+        fldnm = char(flds(fld));
+        if ~strcmpi(fldnm,'predictions')
+            PRT.model(mid).output.fold(f).(fldnm)=model.(fldnm);
+        end
+    end
+end
+
+% Model level statistics (across folds)
+%      ttt       = vertcat(PRT.model(mid).output(k).fold(:).targets);
+%      m.type    = PRT.model(mid).output(k).fold(1).type;
+%      m.predictions = vertcat(PRT.model(mid).output(k).fold(:).predictions);
+%      %m.func_val   = [PRT.model(mid).output.fold(:).func_val];
+%      gstats        = prt_stats(m,ttt(:),nc);
+%     % Model level statistics (across folds) - average across folds
+fnamestats = fieldnames(stats);
+gstats = struct;
+for i=1:length(fnamestats)
+    size_stats = size(PRT.model(mid).output.fold(1).stats.(fnamestats{i}));
+    val = zeros(prod(size_stats),n_folds);
+    for j = 1:n_folds
+        val(:,j) = PRT.model(mid).output.fold(j).stats.(fnamestats{i})(:);
+    end
+    av_stats = reshape(nanmean(val,2),size_stats);
+    gstats = setfield(gstats,fnamestats{i},av_stats);
+end
+% If classifier, get confusion matrix globally
+m.type        = PRT.model(mid).output.fold(1).type;
+if strcmpi(m.type,'classifier')
+    ttt             = vertcat(PRT.model(mid).output.fold(:).targets);
+    m.predictions = vertcat(PRT.model(mid).output.fold(:).predictions);
+    %m.func_val    = [PRT.model(mid).output.fold(:).func_val];
+    temp_stats         = prt_stats(m,ttt(:),nc);
+    gstats.con_mat = temp_stats.con_mat;
+end
+%
+PRT.model(mid).output.stats=gstats;
+
+
+% Save PRT containing machine output
+% -------------------------------------------------------------------------
+if ~isfield(in,'savePRT') || in.savePRT
+    outfile = [prt_dir, filesep,'PRT.mat'];
+    disp('Updating PRT.mat.......>>')
+    if spm_check_version('MATLAB','7') < 0
+        save(outfile,'-V6','PRT');
+    else
+        save(outfile,'PRT');
+    end
+else
+    outfile = PRT;
+end
+
+end
+
+
