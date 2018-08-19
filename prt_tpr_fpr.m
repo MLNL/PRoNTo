b91b02c753fd5a017285d13ce5f5539656ee349d
lilytong20 <tong.wu2@uq.net.au>
2018-08-19 15:15:54 +0100

    Modified prt_tpr_fpr.m
    Changed how to record tpr and fpr due to considerations on cases when repeated test scores are present.

--- prt_tpr_fpr.m
+++ prt_tpr_fpr.m
@@ -16,11 +16,42 @@ numClass = numel(unique(targets(:)));
 if numClass == 2
     % Compute tpr and fpr
     targpos = targets==1;
-    [~,idx] = sort(scores,'descend');
-    s_targpos = targpos(idx);% Sorted targets
+    numPos = sum(targpos);
+    numNeg = sum(~targpos);
+    if numPos<=0 || numNeg<=0
+        error('The number(s) of test positives and/or negatives are incorrect.');
+    end
+    [s_scores,idx] = sort(scores,'descend');
+    s_targets = targets(idx);% Sorted targets
+    
+    tp = 0;
+    fp = 0;
+    thr_prev = s_scores(1)+1; % For (0,0) in ROC space
+    num_scores = length(s_scores);
+    num_thr = length(unique(s_scores))+1;
+    tpr = [];
+    fpr = [];
+    i = 1;
+    j = 1;
+    while i<=num_scores && j<=num_thr
+        if thr_prev~=s_scores(i)
+            tpr(j,1) = tp/numPos;
+            fpr(j,1) = fp/numNeg;
+            thr_prev = s_scores(i);
+            j = j+1;
+        end
+        
+        if s_targets(i)==1
+            tp = tp+1;
+        else
+            fp = fp+1;
+        end
+        
+        i = i+1; 
+    end
+    tpr(j,1) = tp/numPos; % (1,1) in ROC space
+    fpr(j,1) = fp/numNeg;
 
-    tpr      = cumsum(single(s_targpos))/sum(single(s_targpos));
-    fpr      = cumsum(single(~s_targpos))/sum(single(~s_targpos));
     
     if nnz(targpos)<2 || nnz(~targpos)<2
         % Cannot compute tpr or fpr if not enough points
@@ -28,14 +59,6 @@ if numClass == 2
         fpr = NaN;
     end
     
-    % Deal with tails, avoiding repeated values
-    if tpr(1) ~= 0
-        tpr      = [0 ; tpr];
-        fpr      = [0 ; fpr];
-    elseif tpr(end) ~= 1
-        tpr      = [tpr ; 1];
-        fpr      = [fpr ; 1];
-    end
         
 else
     % Cannot compute tpr and fpr is not both classes are present or if not
